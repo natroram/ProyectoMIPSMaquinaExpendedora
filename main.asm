11f68783a8aa082ab91c6becd34c0d805a3c780b
eapb99 <emaalpar@espol.edu.ec>
2021-11-27 01:05:21 -0500

    More Functions

--- main.asm
+++ main.asm
@@ -16,30 +16,38 @@
 	titulo: .asciiz "******Tenemos estas opciones******\n"	
 	stock: .asciiz "BAJO STOCK"
 	string: .asciiz ".- "
+	flag: .asciiz "SI"
+        buffer: .space 4
+        labelmanejador: .asciiz "Desea seguir ingresando monedas/billetes: \n 1)Si \n 2)No \n"
+        labelmoneda: .asciiz "Ingrese una denominacion de dinero valida: \n"
+        labelcantidad: .asciiz "Ingrese el numero de monedas/billetes que tiene: \n "
+
 .text 
 	main :
-		#j menu	
-		addi $t0,$zero,20
-		mtc1 $t0,$f0
-		cvt.s.w $f0,$f0
+		#addi $t0,$zero,100
+		#mtc1 $t0,$f0
+		#cvt.s.w $f0,$f0
 		
-		jal contains		
+		#jal contains		
 
-		move $t8,$v0	
+#		move $t8,$v0	
 				
 		li $v0,1
 		move $a0,$t8
 		syscall
-
-#		li $v0, 4
-#		la $a0, presentacion
-#		syscall		
-
+		
+		jal manejador
+		mtc1 $v0,$f3
+		
+		li $v0, 2
+		mov.s $f12, $f3
+		syscall 		
+		
 		j exit
 		
-		
-		
 	exit:
+		li $v0,10
+		syscall	
 
 
 	
@@ -48,8 +56,7 @@ checkStock:
   sw $ra,0($sp)
   sw $t0,4($sp)
   s.s $f2,8($sp)
-  s.s $f3,12($sp)
-  
+  s.s $f3,12($sp) 
   addi $t0,$zero,100
   mtc1 $t0,$f2
   cvt.s.w $f2,$f2
@@ -65,10 +72,6 @@ checkStock:
   jr $ra
 
 
-
-
-
-
 menu: 
 	li $v0, 4
 	la $a0, presentacion
@@ -77,7 +80,6 @@ menu:
 	la $a0, titulo
 	syscall
 	j whilemenu	
-
 
 whilemenu:
 	addi $t1,$t1,1
@@ -162,34 +164,159 @@ else:
 		
 		j whilemenu
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#LISTO
 contains:
-  addi $sp,$sp,24
+  addi $sp,$sp,-20
   sw $ra,0($sp)
   sw $t0,4($sp) # 1
   sw $t1,8($sp) # -1
   sw $t2,12($sp) # i
-  sw $t3,16($sp) # i
-  s.s $f1,20($sp)  
+  s.s $f1,16($sp)  
   addi $t3,$zero,-1
-  j loop
+  j loopcontains
+
+loopcontains: 	  
+  	beq $t2,36, freecontains # CAMBIAR ETIQUETA Y OBTENER EL LEN
+	l.s $f1,monedas($t2)   
+	addi $t2, $t2,4 
+	c.eq.s $f0,$f1
+	bc1t ifcontains
+	j loopcontains
+	
+ifcontains:
+        addi $t3,$zero,1
+       	j freecontains
+
+freecontains:
+  move $v0,$t3
   lw $ra,0($sp)
   lw $t0,4($sp)
   lw $t1,8($sp)
   lw $t2,12($sp)
-  lw $t3,16($sp)
-  l.s $f1,20($sp)
-  addi $sp,$sp,24
+  l.s $f1,16($sp)
+  addi $sp,$sp,20
   jr $ra  
+
 			
-loop: 	  
-   	beq $t2,36,exitl # CAMBIAR ETIQUETA Y OBTENER EL LEN
-	l.s $f1,monedas($t2)   
-	addi $t2, $t2,4 
-	c.eq.s $f0,$f1
-	bc1t if
-if:
-        addi $t3,$zero,1
-       	j loop
-	
-			
-exitl: 
+						
+
+manejador:		
+   addi $sp,$zero,-20
+   sw $ra,0($sp)
+   sw $s0,4($sp)
+   sw $s1,8($sp)
+   sw $s2,12($sp)
+   s.s $f1,16($sp)  
+   
+   addi $t0,$zero,1
+   addi $t1,$zero,1
+   
+   addi $t9,$zero,0
+   mtc1 $t9,$f1
+   cvt.s.w $f1,$f1
+   
+   j whilemanejador
+   
+ 
+whilemanejador:
+   bne $t0,$t1,freemanejador
+      
+   addi $s0,$zero,1 # reservar
+   addi $s1,$zero,-1 # reservar
+   addi $s2,$zero,-1 # reservar
+   
+    
+   
+   j whilecheckcontains   
+   
+
+input:
+   li $v0, 4
+   la $a0, labelmanejador
+   syscall   
+   
+   li $v0, 5
+   syscall
+    
+   move $t1, $v0	      
+   j whilemanejador
+   
+   
+
+freemanejador:
+ 
+   mfc1 $v0,$f1
+   
+   lw $ra,0($sp)
+   lw $s0,4($sp)
+   lw $s1,8($sp)
+   lw $s2,12($sp)
+   l.s $f1,16($sp)  
+   addi $sp,$sp,20
+    
+   jr $ra
+ 
+ 
+
+whilecheckcontains:
+   beq $s0,$s1,whilecantidad #next while
+   
+   li $v0, 4
+   la $a0, labelmoneda
+   syscall    
+  
+   li $v0, 6
+   syscall
+  
+
+   
+   jal contains
+   add $s1,$zero, $v0
+       
+   j whilecheckcontains
+   
+ 
+whilecantidad:
+   bgtz $s2, calculos #calculos
+   
+   li $v0, 4
+   la $a0, labelcantidad
+   syscall  
+   
+   li $v0, 5
+   syscall
+   
+   move $s2, $v0
+   
+   j whilecantidad
+
+calculos:
+
+  mtc1 $s2,$f2
+  cvt.s.w $f2,$f2   
+  mul.s $f3,$f0,$f2
+  add.s $f1,$f1,$f3
+  j input
+  
+  
+ 
--- proyecto.c
+++ proyecto.c
@@ -26,30 +26,37 @@ int main(){
                 printf("LA CANTIDAD DE DINERO INGRESADA ES INSUFICIENTE");
                 printf("SU DINERO SERA DEVUELTO: %d",total);
             }
-            valor = input("DESEA COMPRAR OTRO PRODUCTO")
+            
         }
         else {
             printf("ESTE PRODUCTO NO TIENE STOCK, ELIJA OTRO");
-            valor = input("DESEA COMPRAR OTRO PRODUCTO")
         }
+        valor = input("DESEA COMPRAR OTRO PRODUCTO")
+
     }
     return 0;
 }
 
 dineroA = [0.01, 0.05, 0.1, 0.25, 0.5, 1 ,5, 10,20, 50, 100]
 
-def monedas(arreglo):
-    *char valor= "SI"
+def monedas():
+    valor= 1
+    valor2=1
     total =0
-    while (valor == "SI"){
-        moneda=float(input("Ingrese una denominacion de dinero: "))
-        while (moneda not in arreglo){
+    while (valor == valor2){
+        //moneda=float(input("Ingrese una denominacion de dinero: "))
+        //valor = contain(moneda)
+        valorcontains = 1
+        valorcontains2 = -1
+        while (valorcontains != valorcontains2){
             moneda=float(input("Ingrese una denominacion de dinero valida: "))
+            valorcontains2 = contain(moneda) # 1
         }
-        cantidad = int(input("Ingrese el numero de monedas/billetes que tiene: :) "))
+        cantidad = -1
         while (cantidad<=0){
             cantidad = int(input("Ingrese el numero de monedas/billetes que tiene: :) "))
         }
+        # calculos
         total+=(cantidad*moneda)
         valor = input("Desea seguir ingresando monedas/billetes: ")
     }
@@ -84,7 +91,7 @@ int menu(int precios[],char **nombres[],int cantidad[],int copia[]){
 a = []
 int containts(int valor){
     int v= 0
-    for i in l:
+    for(int i=0;i<):
         if (i == valor ) 
           v =1 
         else 
